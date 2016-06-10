39e5166aebed8572cfc60f6a11e81ec816040447
Dan Gorman <danielhgorman@gmail.com>
2016-06-10 13:34:18 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+	attr_accessor :base
+
+	def initialize(base)
+		@base = base
+	end
+
+	def sorted(word)
+		word.chars.sort.join
+	end
+
+	def match(array)
+		array.select {|word| sorted(word) == sorted(base)}
+	end
+end
